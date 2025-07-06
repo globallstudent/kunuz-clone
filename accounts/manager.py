69d4cee60e4db2c5219c0677d98581e5d2478105
Asliddin Abdumannonov <158866300+globallstudent@users.noreply.github.com>
2025-07-06 12:34:59 +0500

    Enforce email for user creation

--- accounts/manager.py
+++ accounts/manager.py
@@ -4,6 +4,11 @@ class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular user with the given email and password."""
+        if not email:
+            raise ValueError("The Email must be set")
+
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
